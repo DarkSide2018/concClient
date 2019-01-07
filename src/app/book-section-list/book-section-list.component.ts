ccd41c69be3f98b8b1efa5dab12cb79157aba622
DefNeoRoman <d89056411060@yandex.ru>
2019-01-08 02:47:08 +0300

    need mongo)))

--- src/app/book-section-list/book-section-list.component.ts
+++ src/app/book-section-list/book-section-list.component.ts
@@ -10,14 +10,19 @@ export class BookSectionListComponent implements OnInit {
 
   private bookSections: Array<any>;
   private bookContents: Array<any>;
-  private displayDesc:boolean = false;
+  // without declaration of array not working
+  private displayDesc:  Array<boolean> = new Array<boolean>();
+
   constructor(private bookSectionService: BookSectionService) {
   }
-  showDesc(){
-    this.displayDesc = !this.displayDesc;
+  showDesc(id){
+   this.displayDesc[id] = !this.displayDesc[id];
+  }
+  checkCondition(id) : boolean{
+
+    return this.displayDesc[id]
   }
   showContent(id){
-    console.log(id);
     this.bookSectionService.getContentByID(id).subscribe(data => {
       this.bookContents = data;
     });
@@ -26,6 +31,10 @@ export class BookSectionListComponent implements OnInit {
     this.bookSectionService.getAll().subscribe(data => {
       this.bookSections = data;
     });
+    this.displayDesc = new Array<boolean>(this.bookSections.length);
+    let i:number;
+    for(i=0;i<this.displayDesc.length;i++) {
+      this.displayDesc[i] = false;
+    }
   }
-
 }
